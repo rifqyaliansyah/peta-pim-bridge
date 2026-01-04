e9bee1859ca808c893f467566f7fe5778d53a824
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-04 12:30:39 +0700

    fix not connect with database in production

--- config/db.js
+++ config/db.js
@@ -2,19 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  // host: process.env.DB_HOST,
-  // port: process.env.DB_PORT,
-  database: process.env.DB_URL,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
-});
-
-pool.on('connect', () => {
-  console.log('Database connected');
-});
-
-pool.on('error', (err) => {
-  console.error('Database error:', err);
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
 });
 
 module.exports = pool;
