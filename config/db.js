a91f4a59b2d2853cd3ae8d9185e4e68eaae2ccb8
Rifqy Aliansyah Assidik <rifqyaliansyah1877@gmail.com>
2026-01-04 12:26:40 +0700

    setup db for production

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  database: process.env.DB_URL,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
 });
 
 pool.on('connect', () => {
